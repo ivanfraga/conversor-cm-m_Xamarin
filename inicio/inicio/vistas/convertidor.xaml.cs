545312195677a3e01e9503d74144749bf5a1a05d
fraga <edwin.fraga@epn.edu.ec>
2022-07-30 21:02:23 -0500

    Agregar archivos de proyecto.

---
+++ inicio/inicio/vistas/convertidor.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace inicio.vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class convertidor : ContentPage
+    {
+        double cm;
+        double m;
+        public convertidor()
+        {
+            InitializeComponent();
+        }
+
+        public void Calcular()
+        {
+            cm = Convert.ToDouble(txtcm.Text);
+            m= cm/ 100;
+            reslbl.Text = m.ToString() + " m";
+        }
+
+        public void validar()
+        {
+            if (!String.IsNullOrEmpty(txtcm.Text))
+            {
+                Calcular();
+            }
+            else{
+                DisplayAlert("Error mijo","ingresa una cantidad", "De one");
+            }
+
+        }
+
+        private void btnVolver_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            validar();
+        }
+    }
+}
